d0aeacfe6b1ceafd052d53fcbf069db34d60fd9d
lavishabhatia <lavishabhatia1012@gmail.com>
2024-03-20 16:18:27 +0530

    edit feature

--- src/components/NoData.jsx
+++ src/components/NoData.jsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 function NoData({ text, onClick }) {
   return (
     <div>
-      <p className="text-center text-base">{text}</p>
+      <p className="text-center text-base text-[#1976D2]">{text}</p>
       <div className="  flex items-center justify-center ">
         {onClick && <Button variant="contained" onClick={onClick}>Add Now</Button>}
       </div>
